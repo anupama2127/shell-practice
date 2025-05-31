118a8a887a4c8e50552d8c6809032a46887d386d
anupamadevi <anupamagantyada.999@gmail.com>
2025-05-31 20:32:20 +0530

    colors

---
+++ 14-logs.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\E[0M"
+LOGS_FOLDER="/var/log/shellscript-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+mkdir -p $LOGS_FOLDER
+echo "script started executing at: $(date)" &>>$LOG_FILE
+
+
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N" &>>$LOG_FILE
+    exit 1
+else
+    echo "you are running with root access" &>>$LOG_FILE
+fi
+#validate functions takes input as exit status, what command they tried to install
+VALIDATE(){
+    if [ $? -eq 0 ]
+then
+    echo  -e "installing $2 is ... $G SUCCESS $N" &>>$LOG_FILE 
+else
+    echo -e "installing $2 is ... $R FAILURE $N" &>>$LOG_FILE
+fi 
+}
+dnf list installed mysqL &>>$LOG_FILE
+if [ $? -ne 0 ]
+then
+    echo "MYSQL is not installed... going to install it" &>>$LOG_FILE
+    dnf install mysql -y &>>$LOG_FILE
+    VALIDATE $? "MYSQL"    
+else
+    echo -e "Nothing to do MYSQL... $Y already installed $N" &>>$LOG_FILE
+fi  
+dnf list installed python3 &>>$LOG_FILE
+if [ $? -ne 0 ]
+then
+    echo "python3 is not installed... going to install it" &>>$LOG_FILE
+    dnf install python3 -y &>>$LOG_FILE
+    VALIDATE $? "python3"    
+else
+   echo -e "Nothing to do python... $Y already installed $N" &>>$LOG_FILE
+fi
+dnf list installed nginx &>>$LOG_FILE
+ if [ $? -ne 0 ]
+then
+    echo "nginx is not installed... going to install it" &>>$LOG_FILE
+    dnf install nginx -y &>>$LOG_FILE
+    VALIDATE $? "nginx"    
+else
+   echo -e "Nothing to do nginx... $Y already installed $N" &>>$LOG_FILE
+fi  
