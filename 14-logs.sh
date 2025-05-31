5f3881ae5c140d2cc6d410e5aa7bbd83876dd2d2
anupamadevi <anupamagantyada.999@gmail.com>
2025-06-01 01:20:05 +0530

    colors

--- 14-logs.sh
+++ 14-logs.sh
@@ -5,13 +5,12 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\E[0M"
-LOGS_FOLDERS="/var/log/shellscript-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE=$LOGS_FOLDER/$SCRIPT_NAME.log"
+LOGS_FOLDER="/var/log/shellscript-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -fi)
+LOG_FIL=$LOGS_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p $LOGS_FOLDER
-echo "script started executing at: $(date)" | tee -a $LOG_FILE
-
+echo "script started executing at: $(date)"n | tee -a $LOG_FILE
 
 if [ $USERID -ne 0 ]
 then
@@ -24,7 +23,7 @@ fi
 VALIDATE(){
     if [ $? -eq 0 ]
 then
-    echo  -e "installing $2 is ... $G SUCCESS $N " | tee -a $LOG_FILE
+    echo  -e "installing $2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
 else
     echo -e "installing $2 is ... $R FAILURE $N" | tee -a $LOG_FILE
 fi 
@@ -38,21 +37,21 @@ then
 else
     echo -e "Nothing to do MYSQL... $Y already installed $N" | tee -a $LOG_FILE
 fi  
-dnf list installed python3 &>>$LOG_FILE
+dnf list installed python3  &>>$LOG_FILE
 if [ $? -ne 0 ]
 then
     echo "python3 is not installed... going to install it" | tee -a $LOG_FILE
-    dnf install python3 -y &>>$LOG_FILE
+    dnf install python3 -y  &>>$LOG_FILE
     VALIDATE $? "python3"    
 else
    echo -e "Nothing to do python... $Y already installed $N" | tee -a $LOG_FILE
 fi
-dnf list installed nginx &>>$LOG_FIL
+dnf list installed nginx  &>>$LOG_FILE
  if [ $? -ne 0 ]
 then
-    echo "nginx is not installed... going to install it" | tee -a $LOG_FILE
-    dnf install nginx -y &>>$LOG_FIL
+    echo "nginx is not installed... going to install it" | tee -a $LOG_FILE  
+    dnf install nginx -y  &>>$LOG_FILE
     VALIDATE $? "nginx"    
 else
    echo -e "Nothing to do nginx... $Y already installed $N" | tee -a $LOG_FILE
-fi   
+fi  
